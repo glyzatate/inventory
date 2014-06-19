8a0c738c7422c8e532493c7b0f68e528501e8103
Glyza <glyza.garcia@gmail.com>
2014-06-19 15:32:28 +0800

    for side bar menu

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,80 +1,88 @@
+<style type="text/css">
+.classname {
+	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	box-shadow:inset 0px 1px 0px 0px #ffffff;
+	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #ededed), color-stop(1, #dfdfdf) );
+	background:-moz-linear-gradient( center top, #ededed 5%, #dfdfdf 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ededed', endColorstr='#dfdfdf');
+	background-color:#ededed;
+	-webkit-border-top-left-radius:6px;
+	-moz-border-radius-topleft:6px;
+	border-top-left-radius:6px;
+	-webkit-border-top-right-radius:6px;
+	-moz-border-radius-topright:6px;
+	border-top-right-radius:6px;
+	-webkit-border-bottom-right-radius:6px;
+	-moz-border-radius-bottomright:6px;
+	border-bottom-right-radius:6px;
+	-webkit-border-bottom-left-radius:6px;
+	-moz-border-radius-bottomleft:6px;
+	border-bottom-left-radius:6px;
+	text-indent:0;
+	border:1px solid #dcdcdc;
+	display:inline-block;
+	color:#777777;
+	font-family:arial;
+	font-size:15px;
+	font-weight:bold;
+	font-style:normal;
+	height:64px;
+	line-height:64px;
+	width:90px;
+	text-decoration:none;
+	text-align:center;
+	text-shadow:1px 1px 0px #ffffff;
+}
+.classname:hover {
+	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #dfdfdf), color-stop(1, #ededed) );
+	background:-moz-linear-gradient( center top, #dfdfdf 5%, #ededed 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#dfdfdf', endColorstr='#ededed');
+	background-color:#dfdfdf;
+}.classname:active {
+	position:relative;
+	top:1px;
+}</style>
+
 <ul class="nav navbar-nav side-nav">
-	<li><a href="http://tatepublishing.net/pt"><i class="fa fa-road"></i> PT Page</a></li>
-	<?php if( is_login() ){ ?>
-	<li <?php if($current_page === "index") echo "class='active'";?>><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
-	<li <?php if($current_page === "my-hr-info") echo "class='active'";?>><a href="my-hr-info.php"><i class="fa fa-home"></i> My HR Information</a></li>
-	<li <?php if($current_page === "forms") echo "class='active'";?>><a href="forms.php"><i class="fa fa-book"></i> Forms</a></li>
-	<li <?php if($current_page === "leaves-application") echo "class='active'";?>><a href="leaves-application.php"><i class="fa fa-calendar"></i> Leaves Application</a></li>
-	
-	<!--<li <?php //if($current_page === "dashboard") echo "class='active'";?>><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>	-->
-	
-	<?php if($user->getUserType() < 8 ){ ?>
-	<li <?php if($current_page == "managers" || ($current_page == 'pending-leaves' && !isset($_GET['type']) )) echo "class='active'";?>><a href="managers.php"><i class="fa fa-cog"></i> Manager's Panel</a></li><?php } ?>
-	
-	<?php if($user->isHRAdmin()){ ?>
-		<li <?php if($current_page == "hrpanel"  || ($current_page=='pending-leaves' && isset($_GET['type']) && $_GET['type']=='hr')) echo "class='active'";?>><a href="hrpanel.php"><i class="fa fa-icon-certificate"></i> For HR Personnel</a></li>
-	<?php } ?>	
-	
-	<?php if($user->isFinanceAdmin()){ ?>
-		<li <?php if($current_page == "finance" || ($current_page=='pending-leaves' && isset($_GET['type']) && $_GET['type']=='finance')) echo "class='active'";?>><a href="finance.php"><i class="fa fa-cog"></i> Finance Panel</a></li>
-	<?php } ?>
-	
-	<?php if($user->isSuperAdmin()){ ?>
-	<li <?php if($current_page == "admin" || ($current_page=='pending-leaves' && isset($_GET['type']) && $_GET['type']=='admin' ) ) echo "class='active'";?>><a href="admin.php"><i class="fa fa-cog"></i> Admin Panel</a></li>
-	<li <?php if($current_page == "settings") echo "class='active'";?>><a href="settings.php"><i class="fa fa-wrench"></i> Settings</a></li>
-	<?php } ?>
-	
-	<li class="dropdown <?php if( $current_page == 'knowledgebase' || ( $current_page == 'editor' && !isset($_GET['type']) ) ){ echo ' active'; } ?>">
-		<a id="sidebar_knowledgebase" class="dropdown-toggle hand" data-toggle="dropdown"><i class="fa fa-caret-square-o-down"></i> Knowledge Base <b class="caret"></b>
-		<?php if( $user->isSuperAdmin() OR $user->isHRAdmin() ){ ?>
-			&nbsp;&nbsp;&nbsp;<span style="font-size:11px; text-decoration:underline;" onClick="window.location.href = 'http://employee.tatepublishing.net/editor.php'">Edit</span>
-		<?php } ?>
-		</a>
-		<ul class="dropdown-menu">
-		<?php 
-			//0-HR only; 1-all cebu staff; 2-all US staff; 3-all staff
-			$condition = '';
-			if(!$user->isSuperAdmin() && !$user->isHRAdmin()){
-				$condition .= 'AND ( visibleTo = 3';
-				if( $user->getLocation()=='US')
-					$condition .= ' OR visibleTo = 2';
-				if( $user->getLocation()=='PH')
-					$condition .= ' OR visibleTo = 1';				
-				if( $user->getDeptNum() == 13 )
-					$condition .= ' OR visibleTo = 0';
-				$condition .= ')';
-			}
-		
-			$manuals = $db->selectQuery('knowledgebase', 'id, title', 'active=1 '.$condition.' ORDER BY level');	
-			foreach( $manuals AS $e ):
-				$active = ($current_page == "knowledgebase" && $_GET['id'] == $id)?"class='active'":'';				
-				echo '<a name="'.$e['id'].'"></a>
-					<li '.$active.'>
-						<a title="'.$e['title'].'" href="knowledgebase.php?id='.$e['id'].'">'.$e['title'].'</a>
-					</li>';
-			endforeach;			
-		?>
-		</ul>
-	</li>
-	
-	<li class="dropdown<?php if( $current_page=='editingManual' || ( $current_page=='editor' && isset($_GET['type']) && $_GET['type']=='editing') ){ echo ' active'; } ?>">
-		<a id="sidebar_editing" class="dropdown-toggle hand" data-toggle="dropdown"><i class="fa fa-caret-square-o-down"></i> Editing Manual <b class="caret"></b>
-		<?php if( $user->isSuperAdmin() OR $user->isEditingAdmin() ){ ?>
-		&nbsp;&nbsp;&nbsp;<span style="font-size:11px; text-decoration:underline;" onClick="window.location.href = 'http://employee.tatepublishing.net/editing.php'">Edit</span>
-		<?php } ?>
-		</a>
-		<ul class="dropdown-menu">
-		<?php 
-			$editing = $db->selectQuery('editingCategory', 'catID, catName', 'parentID=0');	
-			foreach( $editing AS $e ):
-				$active = ($current_page == "editing" && $_GET['id'] == $id)?"class='active'":'';				
-				echo '<a name="'.$e['catID'].'"></a>
-					<li '.$active.'>
-						<a title="'.$e['catName'].'" href="editingManual.php?id='.$e['catID'].'">'.$e['catName'].'</a>
-					</li>';
-			endforeach;			
-		?>
-		</ul>
-	</li>
-	<?php } ?>
+<li>
+<div style="padding:10px">
+
+<table cellspacing="100" border="0">
+	<tr >
+		<td  style="padding:5px"><a href="#" class="classname"><img src="img/home.png" style="width:50px; height:50px;" /></a></td>
+		<td  style="padding:5px"><a href="#" class="classname"><img src="img/settings.png" style="width:50px; height:50px;" /></a></td></a></td>
+	</tr>
+	<tr >
+		<td  style="padding:5px"><a href="mouse.php" class="classname"><img src="img/mouse.png" style="width:50px; height:50px;" /></a></td>
+		<td  style="padding:5px"><a href="#" class="classname"><img src="img/keyboard.png" style="width:50px; height:50px;" /></a></td></a></td>
+	</tr>
+	<tr>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/monitor.png" style="width:50px; height:50px;" /></a></td></a></td>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/cpu.png" style="width:50px; height:50px;" /></a></td></a></td>
+	</tr>
+	<tr>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/headset.png" style="width:50px; height:50px;" /></a></td></a></td>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/hardphone.png" style="width:50px; height:50px;" /></a></td></a></td>
+	</tr>
+	<tr>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/laptop.png" style="width:50px; height:50px;" /></a></td></a></td>		
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/mac.png" style="width:50px; height:50px;" /></a></td></a></td>
+	</tr>
+	<tr>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/tablet.png" style="width:50px; height:50px;" /></a></td></a></td>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/avr.png" style="width:50px; height:50px;" /></a></td></a></td>
+	</tr>
+	<tr>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/ups.png" style="width:50px; height:50px;" /></a></td></a></td>
+		<td style="padding:5px"><a href="#" class="classname"><img src="img/printer.png" style="width:50px; height:50px;" /></a></td></a></td>
+	</tr>
+
+	</table>
+
+
+</div>
+</li>
 </ul>
+
+	
