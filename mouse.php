c67cc45eaaf7f3cba56259914e56c40d003bf2b0
Glyza <glyza.garcia@gmail.com>
2014-06-25 15:07:16 +0800

    list of mouse

--- mouse.php
+++ mouse.php
@@ -8,12 +8,35 @@ if(!is_login()){
 }else{
 	echo '<p>Welcome <b>'.$user->getFullName().' </b>!</p>';		
 	?>
-	mouse here
+	
+	<input type="submit" value="Add Mouse" /> <br/><hr>
+	<table border="1" width="750px">
+		<tr align="center">
+			<th width="25%">Invoice No.</th>
+			<th width="25%">Mouse Brand</th>
+			<th width="25%">Mouse Model</th>
+			<th width="25%">Mouse Serial</th>
+		</tr>
+	</table>	
+	<div style="overflow:auto;height:600px;">
+	<table border="1" width="750px">
+		<?php 
+			$mouse = $db->selectQuery("mouse","*", "1");			
+			foreach( $mouse AS $c ):
+				echo '<tr >';
+				// echo "<td> </td>";
+				echo "<td width='25.2%'>".$c['mouse_brand']."</td>";
+				echo "<td>".$c['mouse_brand']."</td>";
+				echo "<td>".$c['mouse_model']."</td>";
+				echo "<td>".$c['mouse_serial']."</td>";
+				echo '</tr>';				
+			endforeach;
+		?>				
+	</table>
+	</div>
+
+
 	
 	
 <?php }  require "includes/footer.php"; ?>
-<script type="text/javascript">
-	function nRead( nID ){
-		window.location.href="http://employee.tatepublishing.net/index.php?nRead="+nID;
-	}
-</script>
+
